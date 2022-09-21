ea1064be1d7e97f6a49215ff62335299a3ef54e3
Göktuğ Serdar Yıldırım <gokseryil@gmail.com>
2022-09-21 15:09:01 +0300

    added function that finds average of array

--- assignment2.java
+++ assignment2.java
@@ -1,1 +1,20 @@
+public class assignment2 {
+    
+    public static double findAverage(int [] arr){
+        double average = 0;
+        double sum = 0; 
+        
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        average = sum / arr.length;
+        return average;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-1,5,7,10,-7};
+        System.out.println(findAverage(arr));
+
+}
+}
 
