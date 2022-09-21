40afa7d294b1cca7bd9097ccb7f9bd09acbc48d2
Göktuğ Serdar Yıldırım <gokseryil@gmail.com>
2022-09-21 15:32:42 +0300

    added function that sum even or odd indexs

--- assignment2.java
+++ assignment2.java
@@ -11,10 +11,24 @@ public class assignment2 {
         return average;
     }
 
-    public static void main(String[] args) {
-        int[] arr = {-1,5,7,10,-7};
-        System.out.println(findAverage(arr));
-
-}
+    public static int sumOdd(int[] arr){
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(i % 2 != 0 ){
+                sum += arr[i];
+            }
+        }
+        return sum;
+    }
+    
+    public static int sumEven(int[] arr){
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(i % 2 == 0 ){
+                sum += arr[i];
+            }
+        }
+        return sum;
+    }
 }
 
